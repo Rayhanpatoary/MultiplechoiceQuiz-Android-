914a3f4d559c191673eb779f44286c3266aa8472
Rayhanpatoary <mailforfindme@gmail.com>
2019-03-27 14:57:36 -0700

    Desigh 2 Layout

--- app/src/main/java/com/example/rayhanpatoary/multiplequiz/MainActivity.java
+++ app/src/main/java/com/example/rayhanpatoary/multiplequiz/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.rayhanpatoary.multiplequiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button starting_button = (Button)findViewById(R.id.button1);
+        starting_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_question_activity();
+            }
+        });
+    }
+
+    private void start_question_activity() {
+        Intent intent = new Intent(MainActivity.this,Questions.class);
+        startActivity(intent);
     }
 }
